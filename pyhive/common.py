aa9cae7467e6f5209818cafd55073488118286c3
abhishekpathania <abhishek@iomete.com>
2025-01-17 15:02:31 +0530

    Fix Collection Issue

--- pyhive/common.py
+++ pyhive/common.py
@@ -245,7 +245,7 @@ class ParamEscaper(object):
             return self.escape_number(item)
         elif isinstance(item, basestring):
             return self.escape_string(item)
-        elif isinstance(item, collections.Iterable):
+        elif isinstance(item, collections.abc.Iterable):
             return self.escape_sequence(item)
         elif isinstance(item, datetime.datetime):
             return self.escape_datetime(item, self._DATETIME_FORMAT)
